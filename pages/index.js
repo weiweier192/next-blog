4af096fef97b4acc24c504c55ef5b6abd2433e9c
weiweier192 <Aweiweier192@163.com>
2020-05-04 10:57:08 +0800

    '按需引入完成，babel的plugins中要多加一个数组'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+import {Button} from 'antd'
+// import App from './_app.js'
 
 const Home = () => {
   return (
@@ -7,6 +9,10 @@ const Home = () => {
       <Head>
         <title>Home</title>
       </Head>
+      <div>
+        <button>按需引入</button>
+        <Button type="primary">按需引入</Button>
+      </div>
     </>
   )
 }
